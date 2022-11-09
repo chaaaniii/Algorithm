adc82827043050ce50e041572074a03c82a5d826
chaaaniii <104333938+chaaaniii@users.noreply.github.com>
2022-11-09 20:57:45 +0900

    [level 0] Title: 짝수의 합, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -1,7 +1,7 @@
 function solution(n) {
-    let odd = 0;
+    let even = 0;
     for( let i = 0; i <= n; i++ ){
-        if(i % 2 === 0) odd += i;
+        if(i % 2 === 0) even += i;
     }
-    return odd;
+    return even;
 }
