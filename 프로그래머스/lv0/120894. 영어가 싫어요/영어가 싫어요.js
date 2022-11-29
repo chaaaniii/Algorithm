c1b9311a9c9db9a9c2184c7ddf08ee2629b08b61
Chaaaniii <104333938+chaaaniii@users.noreply.github.com>
2022-11-29 21:48:44 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.js
@@ -1,5 +1,13 @@
 function solution(numbers) {
-  const nums = { zero: 0, one: 1, two: 2, three: 3, four: 4, five: 5, six: 6, seven: 7, eight: 8, nine: 9 };
-  const regex = new RegExp(Object.keys(nums).join('|'), 'g');
-  return +numbers.replace(regex, key => nums[key]);
+    numbers = numbers.replace(/one/g, 1);
+    numbers = numbers.replace(/two/g, 2);
+    numbers = numbers.replace(/three/g, 3);
+    numbers = numbers.replace(/four/g, 4);
+    numbers = numbers.replace(/five/g, 5);
+    numbers = numbers.replace(/six/g, 6);
+    numbers = numbers.replace(/seven/g, 7);
+    numbers = numbers.replace(/eight/g, 8);
+    numbers = numbers.replace(/nine/g, 9);
+    numbers = numbers.replace(/zero/g, 0);
+    return numbers*1
 }
