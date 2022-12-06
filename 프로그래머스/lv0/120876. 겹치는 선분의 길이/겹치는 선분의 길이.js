4b3e789767b57d813514fa93b50639cf24427d38
Chaaaniii <104333938+chaaaniii@users.noreply.github.com>
2022-12-06 20:36:37 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.js
@@ -1,9 +1,23 @@
 function solution(lines) {
-    let line = new Array(200).fill(0);
+  const linesArr = [...lines[0], ...lines[1], ...lines[2]]
+  const min = Math.min(...linesArr)
+  const max = Math.max(...linesArr)
 
-    lines.forEach(([a, b]) => {
-        for(; a < b; a++) line[a+100]++;
-    });
+  const result = Array(max - min + 1).fill(0)
 
-    return line.reduce((a, c) =>  c > 1 ? a + 1 : a, 0)
+  for (let line of lines) {
+    line.sort((a, b) => a - b)
+
+    if (min < 0) {
+      line[0] += Math.abs(min)
+      line[1] += Math.abs(min)
+    }
+
+    for (let i = line[0]; i < line[1]; i ++ ) {
+      result[i] ++;
+    }
+  }
+  
+  return result.filter(x => x > 1).length;  
+
 }
