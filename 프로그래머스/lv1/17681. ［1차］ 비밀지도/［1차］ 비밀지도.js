bc9d616dcac6c667fbcb68319549a7093e882a18
Chaaaniii <104333938+chaaaniii@users.noreply.github.com>
2023-01-06 14:04:09 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,38 +1,7 @@
 function solution(n, arr1, arr2) {
-    var answer = [];
+    return arr1.map((v, i) => addZero(n, (v | arr2[i]).toString(2)).replace(/1|0/g, a => +a ? '#' : ' '));
+}
 
-    const tmp1 = [];
-    const tmp2 = [];
-    const len = Math.max(...arr1).toString(2).length;
-
-    console.log(len)
-
-    for(let i = 0; i< arr1.length; i++){
-        tmp1.push (arr1[i].toString(2))
-        while(tmp1[i].length !== len){
-            tmp1[i] = "0"+tmp1[i]
-        }
-    }
-    console.log(tmp1)
-
-    for(let i = 0; i< arr2.length; i++){
-        tmp2.push (arr2[i].toString(2))
-        while(tmp2[i].length !== len){
-            tmp2[i] = "0"+tmp2[i]
-        }
-    }
-
-    console.log(tmp2)
-    for(let i = 0 ; i < len; i++){
-        let val =""
-        for(let j = 0 ; j < len; j++){
-            tmp1[i][j] == 0 && tmp2[i][j] ==0 ? val += " " : val += "#" 
-        }
-        answer.push(val);
-        val = "";
-    }
-    console.log(answer)
-
-
-    return answer;
+const addZero = (n, s) => {
+    return '0'.repeat(n - s.length) + s;
 }
